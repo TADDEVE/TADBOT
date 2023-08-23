802a999705a7a50762785025cef6103e531ed37a
TADDEVE <4tradingaldia@gmail.com>
2023-08-23 09:58:21 +0200

    cambio carpeta

--- includes/deactivate.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function telegram_bot_deactivate() {
-    // Aquí podrías añadir cualquier tarea de limpieza necesaria durante la desactivación.
-}
-
-// Cambia __FILE__ por la ubicación de tu archivo principal del plugin.
-register_deactivation_hook(__FILE__, 'telegram_bot_deactivate');
-?>
